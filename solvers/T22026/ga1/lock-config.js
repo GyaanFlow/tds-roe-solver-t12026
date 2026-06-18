1d3ff0fef9389c10009c89618523245a36a1d622
Gaurav Tomar <gauravtomar79172@gmail.com>
2026-06-18 16:46:15 +0530

    chore(ga1): remove specified emails from whitelist in lock-config.js

--- solvers/T22026/ga1/lock-config.js
+++ solvers/T22026/ga1/lock-config.js
@@ -4,9 +4,6 @@ export const lockConfig = {
 
   // Whitelist of emails allowed to get programmatic solved answers when locked is true.
   allowedEmails: [
-    '21f1000000@ds.study.iitm.ac.in',
-    '22f2001234@ds.study.iitm.ac.in',
-    'user.test+ga1@example.com',
-    'gauravtomar79172@gmail.com'
+    '21f1000000@ds.study.iitm.ac.in'
   ]
 };
