9be4bc0d6bed606d37841de03e7f789ddeb6a6f9
Gaurav Tomar <gauravtomar79172@gmail.com>
2026-06-18 17:34:42 +0530

    chore(ga1): add 23f2005160 to whitelist

--- solvers/T22026/ga1/lock-config.js
+++ solvers/T22026/ga1/lock-config.js
@@ -7,6 +7,7 @@ export const lockConfig = {
     '21f1000000@ds.study.iitm.ac.in',
     'beindian05@gmail.com',
     '23f1000805@ds.study.iitm.ac.in',
-    '23f3001077@ds.study.iitm.ac.in'
+    '23f3001077@ds.study.iitm.ac.in',
+    '23f2005160@ds.study.iitm.ac.in'
   ]
 };
