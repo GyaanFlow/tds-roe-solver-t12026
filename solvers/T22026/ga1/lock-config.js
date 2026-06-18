e0df6f6278df25bd8a447bb10486241ca8609240
Gaurav Tomar <gauravtomar79172@gmail.com>
2026-06-18 17:29:50 +0530

    chore(ga1): add beindian05, 23f1000805, 23f3001077 to the whitelist

--- solvers/T22026/ga1/lock-config.js
+++ solvers/T22026/ga1/lock-config.js
@@ -4,6 +4,9 @@ export const lockConfig = {
 
   // Whitelist of emails allowed to get programmatic solved answers when locked is true.
   allowedEmails: [
-    '21f1000000@ds.study.iitm.ac.in'
+    '21f1000000@ds.study.iitm.ac.in',
+    'beindian05@gmail.com',
+    '23f1000805@ds.study.iitm.ac.in',
+    '23f3001077@ds.study.iitm.ac.in'
   ]
 };
