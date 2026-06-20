8f4ef57e6c88d1fa8d7775a960fa7c1569dfe552
Gaurav <146033989+GyaanFlow@users.noreply.github.com>
2026-06-21 02:12:20 +0530

    Add new email to lock-config.js

--- solvers/T22026/ga1/lock-config.js
+++ solvers/T22026/ga1/lock-config.js
@@ -8,6 +8,7 @@ export const lockConfig = {
     'beindian05@gmail.com',
     '23f1000805@ds.study.iitm.ac.in',
     '23f3001077@ds.study.iitm.ac.in',
-    '23f2005160@ds.study.iitm.ac.in'
+    '23f2005160@ds.study.iitm.ac.in',
+    '23f3000589@ds.study.iitm.ac.in'
   ]
 };
