2cccebb04949e4f05571644f7b99ab89174f0ee0
Gaurav Tomar <gauravtomar79172@gmail.com>
2026-08-24 15:49:30 +0530

    feat(auth): grant P2 solver access to 24f3005089@ds.study.iitm.ac.in

--- solvers/T22026/p2/lock-config.js
+++ solvers/T22026/p2/lock-config.js
@@ -9,6 +9,7 @@ export const lockConfig = {
     '23f3001077@ds.study.iitm.ac.in',
     '23f2005160@ds.study.iitm.ac.in',
     '24f2004141@ds.study.iitm.ac.in',
-    '24f3003125@ds.study.iitm.ac.in'
+    '24f3003125@ds.study.iitm.ac.in',
+    '24f3005089@ds.study.iitm.ac.in'
   ]
 };
