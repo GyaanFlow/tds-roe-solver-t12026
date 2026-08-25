c06d8aba6487ce0dcd008fa16fcf31b70fff2fde
Gaurav Tomar <gauravtomar79172@gmail.com>
2026-08-26 03:45:28 +0530

    security(p2): lock solver for public users (restricted to whitelisted emails)

--- solvers/T22026/p2/lock-config.js
+++ solvers/T22026/p2/lock-config.js
@@ -1,10 +1,20 @@
 // T2 2026 Project 2 Lock Configuration
 export const lockConfig = {
-  // Set to false to unlock the solver for all users dynamically
-  locked: false,
+  // Set to true to lock the solver for all non-whitelisted emails, or false to unlock it for everyone.
+  locked: true,
 
-  // Whitelist of emails
+  // Whitelist of emails allowed to get programmatic solved answers when locked is true.
   allowedEmails: [
-    '*'
+    '23f1000805@ds.study.iitm.ac.in',
+    '23f3001077@ds.study.iitm.ac.in',
+    '23f2005160@ds.study.iitm.ac.in',
+    '24f2004141@ds.study.iitm.ac.in',
+    '24f3003125@ds.study.iitm.ac.in',
+    '24f3005089@ds.study.iitm.ac.in',
+    'beindian05@gmail.com',
+    '21f1000000@ds.study.iitm.ac.in',
+    '23f3000589@ds.study.iitm.ac.in',
+    '24f2008726@ds.study.iitm.ac.in',
+    '23f2005102@ds.study.iitm.ac.in'
   ]
 };
